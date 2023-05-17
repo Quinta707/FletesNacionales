c7118fd06e622cd81c817e0b28631c12bc156413
Quinta707 <saraieqp30@gmail.com>
2023-05-17 07:53:24 -0600

    Update BASE DE DATOS.sql

--- BASE DE DATOS.sql
+++ BASE DE DATOS.sql
@@ -10,9 +10,11 @@ CREATE SCHEMA equi
 GO
 CREATE SCHEMA flet
 GO
-CREATE SCHEMA acce
-GO
 
+
+--****************************************************--
+--**********************ACCESO************************--
+--****************************************************--
 CREATE TABLE acce.tbRoles(
 	role_Id					INT IDENTITY,
 	role_Nombre				NVARCHAR(100) UNIQUE NOT NULL,
@@ -63,6 +65,7 @@ CREATE TABLE acce.tbUsuarios(
 	user_EsAdmin			BIT,
 	role_Id					INT,
 	empe_Id					INT,
+	user_Url				NVARCHAR(MAX),
 	user_UsuCreacion		INT NOT NULL,
 	user_FechaCreacion		DATETIME NOT NULL CONSTRAINT DF_user_FechaCreacion DEFAULT(GETDATE()),
 	user_UsuModificacion	INT,
@@ -116,6 +119,12 @@ ALTER TABLE [acce].[tbPantallasPorRoles]
 ADD CONSTRAINT FK_acce_tbPantallasPorRoles_acce_tbUsuarios_pantrole_UsuCreacion_user_Id FOREIGN KEY([prol_UsuCreacion]) REFERENCES acce.tbUsuarios([user_Id]),
 	CONSTRAINT FK_acce_tbPantallasPorRoles_acce_tbUsuarios_pantrole_UsuModificacion_user_Id FOREIGN KEY([prol_UsuModificacion]) REFERENCES acce.tbUsuarios([user_Id])
 
+
+--****************************************************--
+--********************GENERALES***********************--
+--****************************************************--
+
+--********** DEPARTAMENTOS ************--
 GO
 CREATE TABLE [gral].[tbDepartamentos](
     depa_Id                     INT IDENTITY(1,1),
@@ -161,7 +170,7 @@ eciv_FechaCreacion				DATETIME NOT NULL CONSTRAINT DF_gral_TbEstadosCiviles_eciv
 eciv_UsuModificacion			INT,
 eciv_FechaModificacion			DATETIME,
 eciv_Estado						BIT NOT NULL CONSTRAINT DF_gral_TbEstadosCiviles_eciv_Estado    DEFAULT(1)
-CONSTRAINT     PK_gral_tbEstadosCiviles_ectv_Id PRIMARY KEY(eciv_Id),
+CONSTRAINT     PK_gral_tbEstadosCiviles_ectv_Id					   PRIMARY KEY(eciv_Id),
 CONSTRAINT     FK_gral_tbEstadosCiviles_UsuCreacion_usua_Id        FOREIGN KEY(eciv_UsuCreacion) REFERENCES acce.tbUsuarios(user_Id),
 CONSTRAINT     FK_gral_tbEstadosCiviles_UsuModificacion_usua_Id    FOREIGN KEY(eciv_UsuModificacion) REFERENCES acce.tbUsuarios(user_Id)
 );
@@ -193,9 +202,184 @@ meto_UsuModificacion		INT ,
 meto_FechaModificacion		DATETIME,
 meto_Estado					BIT CONSTRAINT DF_gral_tbMetodosdePago_meto_Estado DEFAULT(1)
 CONSTRAINT PK_gral_tbMetodosdePago_meto_Id                                  PRIMARY KEY(meto_Id ),
-CONSTRAINT PK_gral_tbMetodosdePago_acce_tbUsuarios_meto_UsuCreacion         FOREIGN KEY(meto_UsuCreacion) REFERENCES acce.tbUsuarios(User_Id),
-CONSTRAINT PK_gral_tbMetodosdePago_acce_tbUsuarios_meto_UsuModificacion     FOREIGN KEY(meto_UsuModificacion) REFERENCES acce.tbUsuarios(User_Id)
+CONSTRAINT FK_gral_tbMetodosdePago_acce_tbUsuarios_meto_UsuCreacion         FOREIGN KEY(meto_UsuCreacion) REFERENCES acce.tbUsuarios(User_Id),
+CONSTRAINT FK_gral_tbMetodosdePago_acce_tbUsuarios_meto_UsuModificacion     FOREIGN KEY(meto_UsuModificacion) REFERENCES acce.tbUsuarios(User_Id)
 );	
+
+
+
+
+--****************************************************--
+--**********************EQUIPO************************--
+--****************************************************--
+
+--********** MARCAS ************--
+GO
+CREATE TABLE equi.tbMarcas(
+marc_Id						INT IDENTITY(1,1),
+marc_Nombre					NVARCHAR(100) NOT NULL,
+marc_UsuCreacion			INT NOT NULL,
+marc_FechaCreacion			DATETIME CONSTRAINT DF_equi_tbMarcas_marc_FechaCreacion DEFAULT(GETDATE()),
+marc_UsuModificacion		INT ,
+marc_FechaModificacion		DATETIME,
+marc_Estado					BIT CONSTRAINT DF_equi_tbMarcas_marc_Estado DEFAULT(1)
+CONSTRAINT PK_equi_tbMarcas_marc_Id                                  PRIMARY KEY(marc_Id),
+CONSTRAINT FK_equi_tbMarcas_acce_tbUsuarios_marc_UsuCreacion         FOREIGN KEY(marc_UsuCreacion) REFERENCES acce.tbUsuarios(User_Id),
+CONSTRAINT FK_equi_tbMarcas_acce_tbUsuarios_marc_UsuModificacion     FOREIGN KEY(marc_UsuModificacion) REFERENCES acce.tbUsuarios(User_Id)
+);
+
+--********** TIPO DE VEHICULO ************--
+GO
+CREATE TABLE equi.tbTipoDeVehiculo(
+tipv_Id						INT IDENTITY(1,1),
+tipv_Descripcion			NVARCHAR(100) NOT NULL,
+tipv_UsuCreacion			INT NOT NULL,
+tipv_FechaCreacion			DATETIME CONSTRAINT DF_equi_TipoDeVehiculo_tipv_FechaCreacion DEFAULT(GETDATE()),
+tipv_UsuModificacion		INT ,
+tipv_FechaModificacion		DATETIME,
+tipv_Estado					BIT CONSTRAINT DF_equi_TipoDeVehiculo_tipv_Estado DEFAULT(1)
+CONSTRAINT PK_equi_TipoDeVehiculo_tipv_Id                                  PRIMARY KEY(tipv_Id),
+CONSTRAINT FK_equi_TipoDeVehiculo_acce_tbUsuarios_tipv_UsuCreacion         FOREIGN KEY(tipv_UsuCreacion) REFERENCES acce.tbUsuarios(User_Id),
+CONSTRAINT FK_equi_TipoDeVehiculo_acce_tbUsuarios_tipv_UsuModificacion     FOREIGN KEY(tipv_UsuModificacion) REFERENCES acce.tbUsuarios(User_Id)
+);
+
+--************ MODELOS ************--
+GO
+CREATE TABLE equi.tbModelos(
+mode_Id						INT IDENTITY(1,1),
+mode_Nombre					NVARCHAR(100) NOT NULL,
+marc_Id						INT NOT NULL,
+tipv_Id						INT NOT NULL,
+mode_UsuCreacion			INT NOT NULL,
+mode_FechaCreacion			DATETIME CONSTRAINT DF_equi_tbModelos_mode_FechaCreacion DEFAULT(GETDATE()),
+mode_UsuModificacion		INT ,
+mode_FechaModificacion		DATETIME,
+mode_Estado					BIT CONSTRAINT DF_equi_tbModelos_mode_Estado DEFAULT(1)
+CONSTRAINT PK_equi_tbModelos_mode_Id                                  PRIMARY KEY(mode_Id),
+CONSTRAINT FK_equi_tbModelos_equi_tbMarcas_marc_Id					  FOREIGN KEY(marc_Id) REFERENCES equi.tbMarcas(marc_Id),
+CONSTRAINT FK_equi_tbModelos_equi_tbTipoDeVehiculo_tipv_Id			  FOREIGN KEY(tipv_Id) REFERENCES equi.tbTipoDeVehiculo(tipv_Id),
+CONSTRAINT FK_equi_tbModelos_acce_tbUsuarios_mode_UsuCreacion         FOREIGN KEY(mode_UsuCreacion) REFERENCES acce.tbUsuarios(User_Id),
+CONSTRAINT FK_equi_tbModelos_acce_tbUsuarios_mode_UsuModificacion     FOREIGN KEY(mode_UsuModificacion) REFERENCES acce.tbUsuarios(User_Id)
+);
+
+
+
+
+
+--****************************************************--
+--**********************EMPRESA***********************--
+--****************************************************--
+
+--********** SUCURSALES ************--
+GO
+CREATE TABLE flet.tbSucursales(
+sucu_Id							INT IDENTITY(1,1),
+sucu_Nombre						NVARCHAR(200)   NOT NULL,
+muni_Id							INT				NOT NULL,
+sucu_Direccion					NVARCHAR(200)   NOT NULL,
+sucu_UsuCreacion				INT             NOT NULL,
+sucu_FechaCreacion				DATETIME        CONSTRAINT DF_flet_tbSucursales_sucu_FechaCreacion DEFAULT(GETDATE()),
+sucu_UsuModificacion			INT,
+sucu_FechaModificacion			DATETIME,
+sucu_Estado						BIT             CONSTRAINT DF_flet_tbSucursales_sucu_Estado DEFAULT (1)
+CONSTRAINT PK_flet_tbSucursales_sucu_Id                                  PRIMARY KEY(sucu_Id),
+CONSTRAINT FK_flet_tbSucursales_gral_tbMunicipios_muni_Id                FOREIGN KEY(muni_Id)                 REFERENCES gral.tbMunicipios(muni_Id),
+CONSTRAINT FK_flet_tbSucursales_acce_tbUsuarios_sucu_UsuCreacion         FOREIGN KEY(sucu_UsuCreacion)         REFERENCES acce.tbUsuarios(User_Id),
+CONSTRAINT FK_flet_tbSucursales_acce_tbUsuarios_sucu_UsuModificacion     FOREIGN KEY(sucu_UsuModificacion)     REFERENCES acce.tbUsuarios(User_Id)
+);
+
+--********** EMPLEADOS ************--
+GO
+CREATE TABLE flet.tbEmpleados(
+empe_Id						INT IDENTITY(1,1),
+empe_Nombres				NVARCHAR(200)	NOT NULL,
+empe_Apellidos				NVARCHAR(200)	NOT NULL,
+empe_Identidad				NVARCHAR(15)	NOT NULL,
+empe_FechaNacimiento		DATE			NOT NULL,
+empe_Sexo					CHAR(1)			NOT NULL,
+eciv_Id					    INT				NOT NULL,
+muni_Id						INT	    		NOT NULL,
+empe_DireccionExacta		NVARCHAR(250)	NOT NULL,
+empe_Telefono				NVARCHAR(20)	NOT NULL,
+sucu_Id						INT				NOT NULL,
+carg_Id						INT				NOT NULL,
+empe_UsuCreacion			INT				NOT NULL,
+empe_FechaCreacion			DATETIME		NOT NULL CONSTRAINT DF_pbli_tbEmpleados_empe_FechaCreacion DEFAULT(GETDATE()),
+empe_UsuModificacion		INT,
+empe_FechaModificacion		DATETIME,
+empe_Estado					BIT				NOT NULL CONSTRAINT DF_pbli_tbEmpleados_empe_Estado DEFAULT(1),
+	
+CONSTRAINT PK_flet_tbEmpleados_empe_Id 										PRIMARY KEY(empe_Id),
+CONSTRAINT CK_flet_tbEmpleados_empe_Sexo									CHECK(empe_sexo IN ('F', 'M')),
+CONSTRAINT FK_flet_tbEmpleados_gral_tbEstadosCiviles_eciv_Id        		FOREIGN KEY(eciv_Id)					    REFERENCES gral.tbEstadosCiviles(eciv_Id),			
+CONSTRAINT FK_flet_tbEmpleados_gral_tbMunicipios_muni_Id					FOREIGN KEY(muni_Id)						REFERENCES gral.tbMunicipios(muni_Id),
+CONSTRAINT FK_flet_tbEmpleados_gral_tbCargos_carg_Id						FOREIGN KEY(carg_Id)						REFERENCES gral.tbCargos(carg_Id),
+CONSTRAINT FK_flet_tbEmpleados_acce_tbUsuarios_empe_UsuCreacion				FOREIGN KEY(empe_UsuCreacion)				REFERENCES acce.tbUsuarios(user_Id),
+CONSTRAINT FK_flet_tbEmpleados_acce_tbUsuarios_empe_UsuModificacion			FOREIGN KEY(empe_UsuModificacion)			REFERENCES acce.tbUsuarios(user_Id),
+CONSTRAINT FK_flet_tbEmpleados_flet_tbSucursales_sucu_Id					FOREIGN KEY(sucu_Id)						REFERENCES flet.tbSucursales(sucu_Id)		
+);
+
+--********** CLIENTES ************--
+GO
+CREATE TABLE flet.tbClientes(
+clie_Id						INT IDENTITY(1,1),
+clie_Nombres				NVARCHAR(200)	NOT NULL,
+clie_Apellidos				NVARCHAR(200)	NOT NULL,
+clie_Identidad				NVARCHAR(15)	NOT NULL,
+clie_FechaNacimiento		DATE			NOT NULL,
+clie_Sexo					CHAR(1)			NOT NULL,
+eciv_Id					    INT				NOT NULL,
+muni_Id						INT	    		NOT NULL,
+clie_DireccionExacta		NVARCHAR(250)	NOT NULL,
+clie_Telefono				NVARCHAR(20)	NOT NULL,
+clie_UsuCreacion			INT				NOT NULL,
+clie_FechaCreacion			DATETIME		NOT NULL CONSTRAINT DF_pbli_tbClientes_clie_FechaCreacion DEFAULT(GETDATE()),
+clie_UsuModificacion		INT,
+clie_FechaModificacion		DATETIME,
+clie_Estado					BIT				NOT NULL CONSTRAINT DF_pbli_tbClientes_clie_Estado DEFAULT(1),
+	
+CONSTRAINT PK_flet_tbClientes_clie_Id 										PRIMARY KEY(clie_Id),
+CONSTRAINT CK_flet_tbClientes_clie_Sexo										CHECK(clie_sexo IN ('F', 'M')),
+CONSTRAINT FK_flet_tbClientes_gral_tbEstadosCiviles_eciv_Id        			FOREIGN KEY(eciv_Id)					    REFERENCES gral.tbEstadosCiviles(eciv_Id),			
+CONSTRAINT FK_flet_tbClientes_gral_tbMunicipios_muni_Id						FOREIGN KEY(muni_Id)						REFERENCES gral.tbMunicipios(muni_Id),
+CONSTRAINT FK_flet_tbClientes_acce_tbUsuarios_UserCreate					FOREIGN KEY(clie_UsuCreacion)				REFERENCES acce.tbUsuarios(user_Id),
+CONSTRAINT FK_flet_tbClientes_acce_tbUsuarios_UserUpdate					FOREIGN KEY(clie_UsuModificacion)			REFERENCES acce.tbUsuarios(user_Id),	
+);	
+
+--********** ITEMS ************--
+GO
+CREATE TABLE flet.tbItems(
+item_Id						INT IDENTITY(1,1),
+item_Nombre					NVARCHAR(100) NOT NULL,
+item_Descripcion			NVARCHAR(100) NOT NULL,
+item_Peso					DECIMAL(18,2) NOT NULL, --En kilogramos(?
+item_Volumen				DECIMAL(18,2) NOT NULL,
+item_UsuCreacion			INT NOT NULL,
+item_FechaCreacion			DATETIME CONSTRAINT DF_equi_tbModelos_mode_FechaCreacion DEFAULT(GETDATE()),
+item_UsuModificacion		INT ,
+item_FechaModificacion		DATETIME,
+item_Estado					BIT CONSTRAINT DF_equi_tbModelos_mode_Estado DEFAULT(1)
+CONSTRAINT PK_flet_tbItems_item_Id                                  PRIMARY KEY(item_Id),
+CONSTRAINT FK_flet_tbItems_acce_tbUsuarios_item_UsuCreacion         FOREIGN KEY(item_UsuCreacion) REFERENCES acce.tbUsuarios(User_Id),
+CONSTRAINT FK_flet_tbItems_acce_tbUsuarios_item_UsuModificacion     FOREIGN KEY(item_UsuModificacion) REFERENCES acce.tbUsuarios(User_Id)
+);
+
+
+--********** PEDIDOS ************--
+
+
+
+--********** PEDIDOS DETALLE ************--
+
+--********** FLETES ************--
+
+--********** FLETES DETALLE ************--
+
+--********** TRAYECTOS ************--
+
+--********** ESCALAS POR TRAYECTO ************--
+
+--********** UBICACION POR FLETE ************--
 
 
 -----------------SOLO TABLAS-----------------
@@ -208,10 +392,10 @@ CONSTRAINT PK_gral_tbMetodosdePago_acce_tbUsuarios_meto_UsuModificacion     FORE
 --Cargos ✓
 
 ------------------EQUIPO---------------------
---Modelos
---Marcas
---Vehiculos
---Tipo de vehiculo
+--Modelos ✓
+--Marcas ✓
+--Vehiculos ✓
+--Tipo de vehiculo ✓
 
 ------------------Empresa--------------------
 --Empleados
