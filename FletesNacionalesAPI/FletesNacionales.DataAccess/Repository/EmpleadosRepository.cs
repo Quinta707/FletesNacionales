968b31bcfb1b2f3b2a6b3bab8267cf718c498095
SRMAZ2023 <phynomo09099@gmail.com>
2023-05-19 16:07:37 -0600

    wewe

--- FletesNacionalesAPI/FletesNacionales.DataAccess/Repository/EmpleadosRepository.cs
+++ FletesNacionalesAPI/FletesNacionales.DataAccess/Repository/EmpleadosRepository.cs
@@ -55,14 +55,14 @@ namespace FletesNacionales.DataAccess.Repository
             parametros.Add("@empe_Apellidos", item.empe_Apellidos, DbType.String, ParameterDirection.Input);
             parametros.Add("@empe_Identidad", item.empe_Identidad, DbType.String, ParameterDirection.Input);
             parametros.Add("@empe_FechaNacimiento", item.empe_FechaNacimiento, DbType.Date, ParameterDirection.Input);
-            parametros.Add("@empe_Sexo", item.empe_Sexo, DbType.Date, ParameterDirection.Input);
+            parametros.Add("@empe_Sexo", item.empe_Sexo, DbType.String, ParameterDirection.Input);
             parametros.Add("@eciv_Id", item.eciv_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@muni_Id", item.muni_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@empe_DireccionExacta", item.empe_DireccionExacta, DbType.String, ParameterDirection.Input);
             parametros.Add("@empe_Telefono", item.empe_Telefono, DbType.String, ParameterDirection.Input);
             parametros.Add("@sucu_Id", item.sucu_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@carg_Id", item.carg_Id, DbType.Int32, ParameterDirection.Input);
-            parametros.Add("@empe_UsuModificacion", 1, DbType.Int32, ParameterDirection.Input);
+            parametros.Add("@empe_UsuCreacion", 1, DbType.Int32, ParameterDirection.Input);
 
             var resultado = db.QueryFirst<int>(ScriptsDataBase.EmpleadosInsert, parametros, commandType: CommandType.StoredProcedure);
 
@@ -90,8 +90,8 @@ namespace FletesNacionales.DataAccess.Repository
             parametros.Add("@empe_Nombres", item.empe_Nombres, DbType.String, ParameterDirection.Input);
             parametros.Add("@empe_Apellidos", item.empe_Apellidos, DbType.String, ParameterDirection.Input);
             parametros.Add("@empe_Identidad", item.empe_Identidad, DbType.String, ParameterDirection.Input);
-            parametros.Add("@empe_FechaNacimiento", item.empe_FechaNacimiento, DbType.Date, ParameterDirection.Input);
-            parametros.Add("@empe_Sexo", item.empe_Sexo, DbType.Date, ParameterDirection.Input);
+            parametros.Add("@empe_FechaNacimiento", item.empe_FechaNacimiento, DbType.String, ParameterDirection.Input);
+            parametros.Add("@empe_Sexo", item.empe_Sexo, DbType.String, ParameterDirection.Input);
             parametros.Add("@eciv_Id", item.eciv_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@muni_Id", item.muni_Id, DbType.Int32, ParameterDirection.Input);
             parametros.Add("@empe_DireccionExacta", item.empe_DireccionExacta, DbType.String, ParameterDirection.Input);
