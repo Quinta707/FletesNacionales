0fc78e55a89a01f39a0b22625d9f4722e6853551
SRMAZ2023 <phynomo09099@gmail.com>
2023-05-24 15:11:29 -0600

    tray

--- template/src/app/shared/model/trayectos.model.ts
+++ template/src/app/shared/model/trayectos.model.ts
@@ -1,23 +1,19 @@
-export class Trayectos{
-
-	tray_Id !: number; 
-	tray_Precio !: number; 
-	tray_Descripcion !: String;
-	muni_Inicio !: number; 
-	muni_InicioNombre !: String;
-	depa_Inicio !: number; 
-	depa_InicioNombre !: String;
-	muni_Final !: number; 
-	muni_FinalNombre !: String;
-	depa_Final !: number; 
-	depa_FinalNombre !: String;
-	tray_UsuCreacion !: number; 
-	tray_FechaCreacion !: String;
-	tray_UsuModificacion !: number; 
-	tray_FechaModificacion !: String;
-	tray_Estado !: String;
-	user_Creacion !: String;
-	user_Modificacion !: String;
-
-
+export class Trayectos {
+    tray_Id!: number;
+    tray_Descripcion!: String;
+    muni_Inicio!: number;
+    muni_InicioNombre!: String;
+    depa_Inicio!: number;
+    depa_InicioNombre!: String;
+    muni_Final!: number;
+    muni_FinalNombre!: String;
+    depa_Final!: number;
+    depa_FinalNombre!: String;
+    tray_UsuCreacion!: number;
+    tray_FechaCreacion!: String;
+    tray_UsuModificacion!: number;
+    tray_FechaModificacion!: String;
+    tray_Estado!: String;
+    user_Creacion!: String;
+    user_Modificacion!: String;
 }
