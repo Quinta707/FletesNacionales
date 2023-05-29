4cfd7c7f7b2fe348d283b34b58bed6767bb0b6e1
SRMAZ2023 <phynomo09099@gmail.com>
2023-05-29 15:56:12 -0600

    si

---
+++ template/src/app/shared/model/trayectos.model.ts
@@ -0,0 +1,46 @@
+export class Trayectos{
+	
+	flet_Id!: number; 
+	vehi_Id!: number;
+	vehi_Placa!: String;
+	mode_Id!: number;
+	mode_Nombre!: String;
+	estp_Id!: number;
+	estp_Nombre!: String;
+	marc_Id!: number;
+	marc_Nombre!: String;
+	empe_Id!: number;
+	empe_NombreCompleto!: String;
+	empe_Identidad!: String;
+	empe_FechaNacimiento!: String;
+	empe_Sexo!: String;
+	eciv_Id!: number;
+	eciv_Descripcion!: String;
+	muni_Id!: String;
+	empe_DireccionExacta!: String;
+	empe_Telefono!: String;
+	sucu_Id!: number;
+	sucu_Nombre!: String;
+	carg_Id!: number;
+	carg_Descripcion!: String;
+	tray_Id!: number;
+	muni_Inicio!: String;
+	muni_Final!: String;
+	muni_NombreInicio!: String;
+	muni_CodigoInicio!: String;
+	muni_NombreFinal!: String;
+	muni_CodigoFinal!: String;
+	flet_FechaDeSalida!: Date;
+	flet_UsuCreacion!: number;
+	flet_FechaCreacion!: String;
+	flet_UsuModificacion!: number;
+	flet_FechaModificacion!: String;
+	flet_Estado!: String;
+	user_Creacion!: String;
+	user_Modificacion!: String;
+	flet_Ubicado!: String;
+	flet_FechaDeSalidaParseada!: Date
+    flet_PedidosTotales !: number;
+    flet_PedidosCompletados !: number;
+
+}
